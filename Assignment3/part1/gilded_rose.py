61decd9d4a4d92ac907675a0935c6cb37f9d1ecd
jiangerdi <erdijiang@gmail.com>
2023-10-04 22:41:08 -0700

    Complete A3

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -13,37 +13,50 @@ class Item:
 
 
 class GildedRose(object):
-
+    """
+    In the refactored code, I applied the "Strategy Pattern", which is a behavioral design pattern.
+    This pattern can define a family of algorithms, encapsulate each one of them, and make them interchangeable.
+    """
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
+    # Increase quality by certain val each day, up to 50
+    def increase_quality_by(self, val, item):
+        item.quality = min(item.quality + val, 50)
+
+    # Drop quality depends on sell date. Once the sell by date has passed, Quality degrades twice as fast. Down to 0.
+    def drop_quality_by(self, val, item, days):
+        if days > 0:
+            item.quality = max(item.quality - val, 0)
+        else:
+            item.quality = max(item.quality - val * 2, 0)
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            # Ignore the Sulfuras items
+            if "Sulfuras" in item.name:
+                continue
+
+            # Aged Brie quality increased by date, up to 50
+            if item.name == "Aged Brie":
+                self.increase_quality_by(1, item)
+            # Check different cases for backstage passes
+            elif "Backstage passes" in item.name:
+                if item.sell_in <= 0:
+                    item.quality = 0
+                elif item.sell_in <= 5:
+                    self.increase_quality_by(3, item)
+                elif item.sell_in <= 10:
+                    self.increase_quality_by(2, item)
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.increase_quality_by(1, item)
+            # Conjured items
+            elif "Conjured" in item.name:
+                self.drop_quality_by(2, item, item.sell_in)
+            # General cases
+            else:
+                self.drop_quality_by(1, item, item.sell_in)
+
+            # decrease sell date by 1 each day
+            item.sell_in -= 1
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -5,11 +5,76 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
+    # Refactor this example test case
     def test_foo(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+
+    def helper(self, items, days):
+        for day in range(days):
+            print("-------- day %s --------" % day)
+            print("name, sellIn, quality")
+            for item in items:
+                print(item)
+            print("")
+            GildedRose(items).update_quality()
+
+    # Test the quality degrades twice as fast as date passed
+    def test_past_date(self):
+        items = [Item(name="+5 Dexterity Vest", sell_in=3, quality=20)]
+        days = 6
+        self.helper(items, days)
+        self.assertEquals(-3, items[0].sell_in)
+        self.assertEquals(11, items[0].quality)
+
+    def test_quality_non_negative(self):
+        items = [Item(name="+5 Dexterity Vest", sell_in=5, quality=3)]
+        days = 5
+        self.helper(items, days)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
+
+    def test_aged_brie(self):
+        items = [Item(name="Aged Brie", sell_in=5, quality=0)]
+        days = 5
+        self.helper(items, days)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(5, items[0].quality)
+
+    def test_quality_upper_limit(self):
+        items = [Item(name="Aged Brie", sell_in=5, quality=49)]
+        days = 5
+        self.helper(items, days)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(50, items[0].quality)
+
+    def test_sulfuras(self):
+        items = [
+                 Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+                 Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+                ]
+        days = 3
+        self.helper(items, days)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(-1, items[1].sell_in)
+        self.assertEquals(80, items[0].quality)
+        self.assertEquals(80, items[1].quality)
+
+    def test_backstage_passes(self):
+        items = [Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=30)]
+        days = 17
+        self.helper(items, days)
+        self.assertEquals(-2, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
+
+    def test_conjured(self):
+        items = [Item(name="Conjured Mana Cake", sell_in=3, quality=6)]
+        days = 3
+        self.helper(items, days)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
 
 
 if __name__ == '__main__':
